9c1d856dbc8384a46b6ecf663ec9dac336db77dd
T-toh <waboit55@gmail.com>
2023-02-18 06:05:05 -0800

    base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - A program printing base 10 numbers
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+
+	num[10];
+
+	i = 0;
+	while
+		(i < 10)
+		{
+			printf(num"%d'\n", i);
+			i++;
+		}
+	return (0);
+}

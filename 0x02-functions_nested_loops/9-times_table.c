e5073e54db723c74e548cac1472d5a1941b38aa4
T-toh <waboit55@gmail.com>
2023-02-22 06:03:19 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,7 +17,7 @@ void times_table(void)
 		while (b <= 9)
 		{
 			rep = a * b;
-			if (b == a)
+			if (b == 0)
 			{
 				_putchar('0' + rep);
 			}
@@ -30,6 +30,11 @@ void times_table(void)
 			{
 				_putchar('0' + rep / 10);
 				_putchar('0' + rep % 10);
+			}
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 			b++;
 		}

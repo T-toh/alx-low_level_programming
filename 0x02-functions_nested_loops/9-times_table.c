22ec1fc8ebdae2b147261099c73846b90fa28621
T-toh <waboit55@gmail.com>
2023-02-22 05:25:36 -0800

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - print times table
+ * Return:void
+ */
+
+void times_table(void)
+{
+	int a = 0;
+	int b;
+	int rep;
+
+	while (a <= 9)
+	{
+		b = 0;
+		while (b <= 9)
+		{
+			rep = a * b;
+			if (b == a)
+			{
+				_putchar('0' + rep);
+			}
+			else if (rep < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + rep);
+			}
+			else
+			{
+				_putchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
+			}
+			b++;
+		}
+		_putchar('\n');
+		a++;
+	}
+}

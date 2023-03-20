a3e1ae6972958969520959f925d5b72e12c21250
T-toh <waboit55@gmail.com>
2023-03-20 13:24:28 -0700

    task 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,18 +1,18 @@
 #include "dog.h"
 #include <stdlib.h>
+
 /**
- * free_dog - check the code for Holberton School students.
- * @d: decimal
- * Return: Always 0.
+ * free_dog - frees dogs.
+ * @d: struct dog.
+ *
+ * Return: no return.
  */
 void free_dog(dog_t *d)
 {
 	if (d)
 	{
-		if (d->name)
-			free(d->name);
-		if (d->owner)
-			free(d->owner);
+		free(d->name);
+		free(d->owner);
 		free(d);
 	}
 }
